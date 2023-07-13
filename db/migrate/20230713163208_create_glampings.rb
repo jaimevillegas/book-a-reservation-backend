06b28e301dc36b6f11a7d13727d65edcacdbcbc5
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-13 11:42:05 -0500

    Create Tables, Add data and test database

---
+++ db/migrate/20230713163208_create_glampings.rb
@@ -0,0 +1,13 @@
+class CreateGlampings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :glampings do |t|
+      t.string :name
+      t.string :glamping_type
+      t.text :description
+      t.string :image
+      t.decimal :daily_rate
+
+      t.timestamps
+    end
+  end
+end
