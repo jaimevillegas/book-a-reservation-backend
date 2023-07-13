04d8b4e01e12d458169247a6c5c60724f6ae4deb
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-13 14:58:58 -0500

    Add API endpoint to list all users that can login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/users/list_users', to: 'user#list_users'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
