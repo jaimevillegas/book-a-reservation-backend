74652b75ff339fbf3b39c91b42ff053d97b41867
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-14 11:07:45 -0500

    Add API endpoint to list Glampings

---
+++ app/helpers/glamping_helper.rb
@@ -0,0 +1,2 @@
+module GlampingHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/users/list_users', to: 'user#list_users'
+  get '/glampings/list_glampings', to: 'glamping#list_glampings'
 
   # Defines the root path route ("/")
   # root "articles#index"
