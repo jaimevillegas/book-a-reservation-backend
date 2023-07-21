067b84355707bc5c8269d195fc4eee8a0b62711a
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-07-21 14:56:37 -0500

    Generate the json files

--- spec/requests/api/user_spec.rb
+++ spec/requests/api/user_spec.rb
@@ -2,18 +2,28 @@ require 'swagger_helper'
 
 RSpec.describe '/users', type: :request do
     path '/users/list_users' do
-        get 'List all users' do
+        get 'List users' do
             tags 'Users'
-            produces 'application/json', 'application/xml'
+            consumes 'application/json'
+            produces 'application/json'
 
             response '200', 'users found' do
-                schema type: :object,
-                    properties: {
-                        id: { type: :integer },
-                        name: { type: :string },
-                        email: { type: :string }
-                    },
-                    required: [ 'id', 'name', 'email' ]
+                schema type: :array,
+                    items: {
+                        properties: {
+                            id: { type: :integer },
+                            name: { type: :string },
+                            email: { type: :string },
+                            password: { type: :string }
+                        }
+                    }
 
-                let(:id) { User.create(name: 'foo', email: '  
+                run_test!
+            end
+
+            response '404', 'users not found' do
+                run_test!
+            end
+        end
+    end 
 end
